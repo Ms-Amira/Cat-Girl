625ea48278002b455508b089f90def38248648d8
Mimi <trish21215@yahoo.com>
2023-04-10 17:04:55 -0400

    Add update and delete urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('cats/', views.cats_index, name='index'),
-    path('cats/<int:cat_id>/', views.cats_details, name='details')
+    path('cats/<int:cat_id>/', views.cats_details, name='details'),
+    path('cats/create/', views.CreateCat.as_view(), name='create_cat'),
+    path('cats/<int:pk>/update/', views.UpdateCat.as_view(), name='cats_update'),
+    path('cats/<int:pk>/delete/', views.DeleteCat.as_view(), name='cats_delete'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from django.http import HttpResponse
 
 from .models import Cat
 
 # Create your views here.
+
+class CreateCat(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats/'
+
+class UpdateCat(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class DeleteCat(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
