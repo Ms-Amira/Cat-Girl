65cb7bff9d681fab1973dd5f3e656e115037d0d0
Mimi <trish21215@yahoo.com>
2023-05-04 19:21:56 -0400

    add static files

--- cats/settings.py
+++ cats/settings.py
@@ -125,6 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+]
+
 MEDIA_URL = 'media/'
 
 # Default primary key field type
